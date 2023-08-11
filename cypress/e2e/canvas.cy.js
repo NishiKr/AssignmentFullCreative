c56c11b87a1155e674b5f7b91debe41c2a5d5020
Nishanth <nishikr5@gmail.com>
2023-08-11 17:11:24 +0530

    Hidden google ads

--- cypress/e2e/canvas.cy.js
+++ cypress/e2e/canvas.cy.js
@@ -6,21 +6,25 @@ describe('Test Suite - Assignment One', () => {
     it('Test Case One', () => {
         cy.get('#imageTemp').then($canvas => {
 
+            //hidingGoogleAd for better screenshot - visual testing assert
+            cy.get('[class="adsbygoogle"]').hideElement()
+
             cy.get('[title="Draw a line"]').should('be.visible').click().should('have.class', 'button line clicked')
             cy.drawHorizontalLine($canvas, "topLeft")
             cy.drawVerticalLine($canvas, "topLeft")
+
             //visual testing assert
             cy.compareSnapshot('PlusSign', 0, 0)
 
             cy.get('[title="Draw a rectangle"]').click().should('have.class', 'button rectangle clicked')
             cy.drawRectangle($canvas, "bottomLeft")
-            //visual testing assert
+            //c
             cy.compareSnapshot('PlusSign+Rectangle', 0, 0)
 
             cy.get('[title="Draw an ellipse or a circle"]').click().should('have.class', 'button ellipse clicked')
             cy.drawCircle($canvas, "topRight", 20)
             //visual testing assert
-            cy.compareSnapshot('PlusSign+Rectangle', 0, 0)
+            cy.compareSnapshot('PlusSign+Rectangle+Circle', 0, 0)
 
         })
     })
