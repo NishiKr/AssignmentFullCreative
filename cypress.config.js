51b5dfee1260777f2e2fbd7a6e82ac1b8b3dd0b3
Nishanth <nishikr5@gmail.com>
2023-08-11 17:06:34 +0530

    Included visual testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,17 @@
-const {defineConfig} = require("cypress");
+const { defineConfig } = require("cypress");
+const getCompareSnapshotsPlugin = require('cypress-image-diff-js/dist/plugin');
 
 module.exports = defineConfig({
     viewportWidth: 1366,
     viewportHeight: 768,
     numTestsKeptInMemory: 5,
     e2e: {
+        setupNodeEvents(on, config) {
+            getCompareSnapshotsPlugin(on, config);
+        },
         baseUrl: "http://www.htmlcanvasstudio.com/"
     },
     env: {
         hideXhr: true
     }
-});
+})
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-plugin-xhr-toggle'
+const compareSnapshotCommand = require('cypress-image-diff-js/dist/command');
+compareSnapshotCommand();
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
